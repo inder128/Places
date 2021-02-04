325fed6ad12a0402885b88cbe69b05dc4f5cad37
inderjeet <b18058@students.iitmandi.ac.in>
2021-02-04 20:09:39 +0530

    add ragister login logout

--- App/views.py
+++ App/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
+from django.contrib import messages
 
 def Home(request):
-	return render(request, 'pages/home.html')
+	return render(request, 'pages/home.html', {'places' : [1, 2, 3, 4, 5, 6, 7]})
+
+def NewPlace(request):
+	if(request.user.is_authenticated):
+		return HttpResponse("new place")
+	else:
+		messages.error(request, "Please login first!!!")
+		return redirect('/login')
